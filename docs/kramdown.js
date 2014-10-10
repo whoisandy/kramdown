ce643f948c2c5be0f8dfff8fd41fe777fafcd423
Bhargav Anand <rjn143@gmail.com>
2014-10-10 09:44:43 -0400

    new kramdown build

--- docs/kramdown.js
+++ docs/kramdown.js
@@ -763,6 +763,40 @@ CodeMirror.defineMode('markdown', function(cmCfg, modeCfg) {
 }, 'xml');
 
 CodeMirror.defineMIME('text/x-markdown', 'markdown');
+var listRE = /^(\s*)([*+-]|(\d+)\.)(\s+)/,
+    elistRE = /^(\s*)([*+-]|(\d+)\.)(\s*)$/,
+    unorderedBullets = '*+-';
+
+CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
+  if (cm.getOption('disableInput')) {
+    return CodeMirror.Pass;
+  }
+  var ranges = cm.listSelections(), replacements = [];
+  for (var i = 0; i < ranges.length; i++) {
+    var pos = ranges[i].head, match, ematch;
+    var inList = cm.getStateAfter(pos.line).list !== false;
+
+    if(!ranges[i].empty() || (ematch = cm.getLine(pos.line).match(elistRE))){
+      cm.execCommand('delLineLeft');
+      cm.execCommand('newlineAndIndent');
+      return;
+    }
+
+    if (!ranges[i].empty() || !inList || !(match = cm.getLine(pos.line).match(listRE))) {
+      cm.execCommand('newlineAndIndent');
+      return;
+    }
+
+    var indent = match[1], after = match[4];
+    var bullet = unorderedBullets.indexOf(match[2]) >= 0
+      ? match[2]
+      : (parseInt(match[3], 10) + 1) + '.';
+
+    replacements[i] = '\n' + indent + bullet + after;
+  }
+
+  cm.replaceSelections(replacements);
+};
 var isMac =  /Mac/.test(navigator.platform);
 
 var shortcuts = {
@@ -1682,40 +1716,6 @@ Kramdown.prototype._action = function(name){
 
 Kramdown.prototype._exec = function(action){
   action.call(this, arguments);
-};
-var listRE = /^(\s*)([*+-]|(\d+)\.)(\s+)/,
-    elistRE = /^(\s*)([*+-]|(\d+)\.)(\s*)$/,
-    unorderedBullets = '*+-';
-
-CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
-  if (cm.getOption('disableInput')) {
-    return CodeMirror.Pass;
-  }
-  var ranges = cm.listSelections(), replacements = [];
-  for (var i = 0; i < ranges.length; i++) {
-    var pos = ranges[i].head, match, ematch;
-    var inList = cm.getStateAfter(pos.line).list !== false;
-
-    if(!ranges[i].empty() || (ematch = cm.getLine(pos.line).match(elistRE))){
-      cm.execCommand('delLineLeft');
-      cm.execCommand('newlineAndIndent');
-      return;
-    }
-
-    if (!ranges[i].empty() || !inList || !(match = cm.getLine(pos.line).match(listRE))) {
-      cm.execCommand('newlineAndIndent');
-      return;
-    }
-
-    var indent = match[1], after = match[4];
-    var bullet = unorderedBullets.indexOf(match[2]) >= 0
-      ? match[2]
-      : (parseInt(match[3], 10) + 1) + '.';
-
-    replacements[i] = '\n' + indent + bullet + after;
-  }
-
-  cm.replaceSelections(replacements);
 };
 
 global.Kramdown = Kramdown;
