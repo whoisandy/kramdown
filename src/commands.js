3bada9299d787e08e66b07fe653d2f29a4a91b60
Bhargav Anand <rjn143@gmail.com>
2014-10-10 08:38:31 -0400

    removed commands from kramdown prototype, revert back to codemirror

--- src/commands.js
+++ src/commands.js
@@ -1,32 +1,42 @@
 var listRE = /^(\s*)([*+-]|(\d+)\.)(\s+)/,
+    elistRE = /^(\s*)([*+-]|(\d+)\.)(\s*)$/,
     unorderedBullets = '*+-';
 
 if(! window.CodeMirror){
   throw new Error('Kramdown requires CodeMirror as a dependency');
 }
 
-Kramdown.commands = CodeMirror.commands;
-
-Kramdown.commands.newlineAndIndentContinueMarkdownList = function(cm) {
-  if (cm.getOption('disableInput')) {
-    return CodeMirror.Pass;
-  }
-  var ranges = cm.listSelections(), replacements = [];
-  for (var i = 0; i < ranges.length; i++) {
-    var pos = ranges[i].head, match;
-    var inList = cm.getStateAfter(pos.line).list !== false;
-
-    if (!ranges[i].empty() || !inList || !(match = cm.getLine(pos.line).match(listRE))) {
-      cm.execCommand('newlineAndIndent');
-      return;
+try {
+  CodeMirror.commands.newlineAndIndentContinueMarkdownList = function(cm) {
+    if (cm.getOption('disableInput')) {
+      return CodeMirror.Pass;
     }
-    var indent = match[1], after = match[4];
-    var bullet = unorderedBullets.indexOf(match[2]) >= 0
-      ? match[2]
-      : (parseInt(match[3], 10) + 1) + '.';
+    var ranges = cm.listSelections(), replacements = [];
+    for (var i = 0; i < ranges.length; i++) {
+      var pos = ranges[i].head, match, ematch;
+      var inList = cm.getStateAfter(pos.line).list !== false;
 
-    replacements[i] = '\n' + indent + bullet + after;
-  }
+      if(!ranges[i].empty() || (ematch = cm.getLine(pos.line).match(elistRE))){
+        cm.execCommand('delLineLeft');
+        cm.execCommand('newlineAndIndent');
+        return;
+      }
 
-  cm.replaceSelections(replacements);
-};
+      if (!ranges[i].empty() || !inList || !(match = cm.getLine(pos.line).match(listRE))) {
+        cm.execCommand('newlineAndIndent');
+        return;
+      }
+
+      var indent = match[1], after = match[4];
+      var bullet = unorderedBullets.indexOf(match[2]) >= 0
+        ? match[2]
+        : (parseInt(match[3], 10) + 1) + '.';
+
+      replacements[i] = '\n' + indent + bullet + after;
+    }
+
+    cm.replaceSelections(replacements);
+  };
+} catch (err) {
+  console.warn(err.message);
+}
